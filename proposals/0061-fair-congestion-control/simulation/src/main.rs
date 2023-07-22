8c5374c22bf0e85b3047ddff81ca394e71aaa47a
Ryo Onodera <ryoqun@gmail.com>
2023-07-22 16:20:24 +0900

    save 2023y07m22d16h20m24s325105825ns

--- proposals/0061-fair-congestion-control/simulation/src/main.rs
+++ proposals/0061-fair-congestion-control/simulation/src/main.rs
@@ -419,7 +419,7 @@ mod tests {
         let tx = Tx::new(3, 100, 3_000_000, vec![Addr(7)]);
         assert_eq!(tracker.start_measuring(&tx), Ok(()));
         assert_eq!(tracker.stop_measuring(&tx, Ok(())), Ok(()));
-        assert_eq!(tracker.fee_markets.get(&Addr(7)).unwrap().reserved_fee, 599004380);
+        assert_eq!(tracker.fee_markets.get(&Addr(7)).unwrap().reserved_fee, 299501145);
         let tx = Tx::new(3, 100, 0, vec![Addr(7)]);
         assert_eq!(tracker.start_measuring(&tx), Ok(()));
         assert_eq!(tracker.stop_measuring(&tx, Ok(())), Ok(()));
