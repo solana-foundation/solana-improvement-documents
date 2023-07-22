d48ac6a09730c12a5f1421c4188f2ae8aed7dd0b
Ryo Onodera <ryoqun@gmail.com>
2023-07-22 15:49:16 +0900

    save 2023y07m22d15h49m16s942399131ns

--- proposals/0061-fair-congestion-control/simulation/src/main.rs
+++ proposals/0061-fair-congestion-control/simulation/src/main.rs
@@ -507,6 +507,6 @@ mod tests {
         let mut tracker = BaseFeeTracker::<{ Policy::new() }>::default();
         let cu = 200;
         let tx = Tx::new(3, cu, 1002600 / cu, vec![Addr(7)]);
-        assert_eq!(tracker.stop_measuring(&tx, Ok(())), Ok(()));
+        assert_eq!(tracker.stop_measuring(&tx, Ok(())), Err(NotMeasured));
     }
 }
