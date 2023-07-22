96ba8c446b5220d5ca55b3aebc605b2848f78d7b
Ryo Onodera <ryoqun@gmail.com>
2023-07-22 15:41:24 +0900

    save 2023y07m22d15h41m24s037385125ns

--- proposals/0061-fair-congestion-control/simulation/src/main.rs
+++ proposals/0061-fair-congestion-control/simulation/src/main.rs
@@ -462,5 +462,9 @@ mod tests {
         assert_eq!(tracker.start_measuring(&tx), Ok(()));
         assert_eq!(tracker.stop_measuring(&tx, Ok(())), Ok(()));
         assert_eq!(tracker.nonconflicting_group_count, 1);
+        let tx = Tx::new(3, cu, 1002600 / cu, vec![Addr(8)]);
+        assert_eq!(tracker.start_measuring(&tx), Ok(()));
+        assert_eq!(tracker.stop_measuring(&tx, Ok(())), Ok(()));
+        assert_eq!(tracker.nonconflicting_group_count, 2);
     }
 }
