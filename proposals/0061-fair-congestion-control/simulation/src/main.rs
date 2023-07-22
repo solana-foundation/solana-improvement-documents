25d64a9492fb4080a26e796b2b2ead0bbc2b238c
Ryo Onodera <ryoqun@gmail.com>
2023-07-22 15:27:45 +0900

    save 2023y07m22d15h27m45s499307556ns

--- proposals/0061-fair-congestion-control/simulation/src/main.rs
+++ proposals/0061-fair-congestion-control/simulation/src/main.rs
@@ -356,12 +356,27 @@ mod tests {
         assert_eq!(tracker.stop_measuring(&tx1, Ok(())), Ok(()));
 
         assert_eq!(tracker.start_measuring(&tx1), Err(InsufficientSuppliedFee(1002600, 1005200)));
+
         assert_eq!(tracker.start_measuring(&tx2), Ok(()));
         assert_eq!(tracker.stop_measuring(&tx2, Ok(())), Ok(()));
+    }
+
+    #[test]
+    fn tracker_cool_down() {
+        let mut tracker = BaseFeeTracker::<{ Policy::new() }>::default();
+        let tx1 = Tx::new(3, 200, 1002600/200, vec![Addr(7)]);
+        let tx2 = Tx::new(4, 200, 1002600/200, vec![Addr(8)]);
+        assert_eq!(tracker.start_measuring(&tx1), Ok(()));
+        assert_eq!(tracker.stop_measuring(&tx1, Ok(())), Ok(()));
+
         assert_eq!(tracker.start_measuring(&tx2), Ok(()));
         assert_eq!(tracker.stop_measuring(&tx2, Ok(())), Ok(()));
 
         assert_eq!(tracker.start_measuring(&tx1), Ok(()));
+        assert_eq!(tracker.stop_measuring(&tx1, Ok(())), Ok(()));
+
+        assert_eq!(tracker.start_measuring(&tx2), Ok(()));
+        assert_eq!(tracker.stop_measuring(&tx2, Ok(())), Ok(()));
     }
 
     #[test]
